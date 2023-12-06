10601beb0679b5a8496ffde7aacdfed2235cfa50
xiron9090 <xiron9090@gmail.com>
2023-12-06 11:37:58 -0500

    add delete verb fort http

--- src/services/rest.base.service.ts
+++ src/services/rest.base.service.ts
@@ -1,38 +1,51 @@
-import axios, { AxiosError } from "axios";
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
+import config from "../../.storybook/main";
 
-async function get<T>(resource: string, params?: T) {
+async function get<T>(resource: string, config?: AxiosRequestConfig) {
   try {
-    return await axios.get(resource, { ...params });
+    return await axios.get(resource, config);
   } catch (error) {
     const axiosError = error as AxiosError;
     return axiosError;
   }
 }
 
-async function put<T>(resource: string, body: T, params?: T) {
+async function put<T>(resource: string, body: T, config?: AxiosRequestConfig) {
   try {
-    return await axios.put(resource, body, { ...params });
+    return await axios.put(resource, body, config);
   } catch (error) {
     const axiosError = error as AxiosError;
     return axiosError;
   }
 }
-async function patch<T, D>(resource: string, body: T, params?: D) {
+async function patch<T>(
+  resource: string,
+  body: T,
+  config?: AxiosRequestConfig
+) {
   try {
-    return await axios.patch(resource, body, { ...params });
+    return await axios.patch(resource, body, config);
   } catch (error) {
     const axiosError = error as AxiosError;
     return axiosError;
   }
 }
 
-async function post<T>(resource: string, body: T) {
+async function post<T>(resource: string, body: T, config?: AxiosRequestConfig) {
   try {
-    return await axios.post(resource, body);
+    return await axios.post(resource, body, config);
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    return axiosError;
+  }
+}
+async function remove(resource: string, config?: AxiosRequestConfig) {
+  try {
+    return await axios.delete(resource, config);
   } catch (error) {
     const axiosError = error as AxiosError;
     return axiosError;
   }
 }
 
-export { get, put, patch, post };
+export { get, put, patch, post, remove };
